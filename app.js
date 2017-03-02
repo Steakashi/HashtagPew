ed2c8eaa1ea44eb8ca8cfd807591dbdc892b3895
Steakashi <newcacao@hotmail.fr>
2017-03-02 19:03:11 +0100

    Twitter stream
    Stream in console all tweets filtered by a given hashtag, defined in config/hashtag.json

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ var fs = require('fs');
 var authFile = fs.readFileSync("config/auth.json");
 var authParam = JSON.parse(authFile);
 
+// Read hashtag
+var hashtagFile = fs.readFileSync("config/hashtag.json");
+var hashtagParam = JSON.parse(hashtagFile);
+
 // Create the Twitter Object
 var twittObject = new Twitter({
 	consumer_key: authParam.consumer_key,
 	consumer_secret: authParam.consumer_secret,
 	access_token_key: authParam.access_token_key,
 	access_token_secret: authParam.access_token_secret
+});
+
+// Stream tweets filtered by given hashtag
+var stream = twittObject.stream('statuses/filter', {track: hashtagParam.hashtag});
+stream.on('data', function(event) {
+  console.log(event && event.text);
+});
+ 
+stream.on('error', function(error) {
+  throw error;
 });
