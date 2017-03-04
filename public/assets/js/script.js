56ffe7907d14a5320b1fae11119872b882306d82
Steakashi <newcacao@hotmail.fr>
2017-03-04 16:45:00 +0100

    Display tweet informations in canvas

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
 
-    var container = document.getElementById('content');
-    
-    socket = io.connect('http://localhost:8080');
+
+
+    var container = document.getElementById('content'),
+   		socket = io.connect('http://localhost:8080');
                     
     socket.on('twitter', function(data) {
     	
-    	console.debug(data);
-    	container.innerHTML = data.user.screen_name;
-       
+    	$('.tweets').prepend('<canvas id="'+data.user.screen_name +'" class="tweet"></canvas>');
+
+    	var canvas = document.getElementById( data.user.screen_name );
+		console.log(canvas);
+		var ctx = canvas.getContext("2d");
+			    	
+		//INIT TEXT CANVAS USER NAME
+		ctx.font = "30px VT323";
+		ctx.fillStyle = "white";
+		ctx.textAlign = "center";
+		ctx.fillText( data.user.screen_name + data.text, canvas.width/2, 40); 
+
+		//INIT IMAGE CANVAS
+		var img = new Image();
+		img.onload = function()
+		{
+		    ctx.drawImage(img, 0, 0, img.width, img.height);
+		};
+					
+	    img.src = data.user.profile_image_url;
     });
 
 });
