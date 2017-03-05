a467ba42cde7f7a6bd4d9e6b5f7b671458ea3356
Steakashi <newcacao@hotmail.fr>
2017-03-05 13:36:45 +0100

    Add style for tweet
    Also implement algorithm to handle line breaks in tweets content

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,63 @@
 $(document).ready(function() {
 
-
-
     var container = document.getElementById('content'),
    		socket = io.connect('http://localhost:8080');
+
+   	function textWrapper(txt, width, margin, ctx){
+
+   		var bufferLine = "", testLine = "", size;
+   		var words = txt.split(' ');
+   		var offset = 0;
+   		width = width - (margin * 2);
+
+   		ctx.fillStyle = "black";
+   		ctx.font = "15px Anonymous Pro";
+
+   		for( var i=0; i<words.length; i++ ){
+   			
+   			bufferLine += words[i];
+
+   			testLine = bufferLine + words[i+1] + " ";
+   			size = ctx.measureText(testLine);
+
+   			if(size.width > width){
+
+   				ctx.fillText( bufferLine, margin, 70+(offset*17) );
+   				offset = offset + 1;
+   				bufferLine = "";
+
+   			}else bufferLine += " ";
+
+   		}
+   	}
                     
     socket.on('twitter', function(data) {
     	
-    	$('.tweets').prepend('<canvas id="'+data.user.screen_name +'" class="tweet"></canvas>');
+    	$('.tweets').prepend('<canvas id="'+data.id +'" class="tweet"></canvas>');
 
-    	var canvas = document.getElementById( data.user.screen_name );
-		console.log(canvas);
+    	var canvas = document.getElementById( data.id );
 		var ctx = canvas.getContext("2d");
-			    	
-		//INIT TEXT CANVAS USER NAME
-		ctx.font = "30px VT323";
-		ctx.fillStyle = "white";
-		ctx.textAlign = "center";
-		ctx.fillText( data.user.screen_name + data.text, canvas.width/2, 40); 
 
-		//INIT IMAGE CANVAS
+		//var padding = $(canvas).css('padding');
+		//padding = padding.substring( 0, padding.length - 2 );
+
+		var padding = 10;
+
+		ctx.fillStyle="rgba(20,20,20, .8)";
+		ctx.fillRect(0,0,canvas.offsetWidth, 48);
+			    	
+		ctx.font = "25px VT323";
+		ctx.fillStyle = "white";
+		ctx.textAlign = "start";
+
+		ctx.fillText( data.user.screen_name, 70, 30); 
+		
+		textWrapper(
+			data.text,
+			canvas.offsetWidth,
+			padding,
+			ctx);
+
 		var img = new Image();
 		img.onload = function()
 		{
