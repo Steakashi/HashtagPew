9ca0ad0891d46c29326a10701e4641bcec5f71d7
Steakashi <newcacao@hotmail.fr>
2017-03-08 22:53:21 +0100

    Display tweets with Pixi.js
    Tweets were previously generated with pure javascript. They're now rendered with Pixi.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use('/', require('./app/routes/default'));
 // Init socket.io event and twitter stream
 io.sockets.on('connection', function(socket) {
 
+	console.log('connected');
  	twittObject.stream('statuses/filter', {'track': hashtagParam.hashtag},
 
 	function(stream) {
@@ -38,4 +39,94 @@ io.sockets.on('connection', function(socket) {
 		});
 
 	});
+
+	socket.on('explosion', function() {
+
+		// Create a new emitter
+		/*var emitter = new PIXI.particles.Emitter(
+
+		    // The PIXI.Container to put the emitter in
+		    // if using blend modes, it's important to put this
+		    // on top of a bitmap, and not use the root stage Container
+		    container,
+
+		    // The collection of particle images to use
+		    [PIXI.Texture.fromImage('image.jpg')],
+
+		    // Emitter configuration, edit this to change the look
+		    // of the emitter
+		    {
+		        alpha: {
+		            start: 0.8,
+		            end: 0.1
+		        },
+		        scale: {
+		            start: 1,
+		            end: 0.3
+		        },
+		        color: {
+		            start: "fb1010",
+		            end: "f5b830"
+		        },
+		        speed: {
+		            start: 200,
+		            end: 100
+		        },
+		        startRotation: {
+		            min: 0,
+		            max: 360
+		        },
+		        rotationSpeed: {
+		            min: 0,
+		            max: 0
+		        },
+		        lifetime: {
+		            min: 0.5,
+		            max: 0.5
+		        },
+		        frequency: 0.008,
+		        emitterLifetime: 0.31,
+		        maxParticles: 1000,
+		        pos: {
+		            x: 0,
+		            y: 0
+		        },
+		        addAtBack: false,
+		        spawnType: "circle",
+		        spawnCircle: {
+		            x: 0,
+		            y: 0,
+		            r: 10
+		        }
+		    }
+		);
+
+		// Calculate the current time
+		var elapsed = Date.now();
+
+		// Update function every frame
+		var update = function(){
+
+		    // Update the next frame
+		    requestAnimationFrame(update);
+
+		    var now = Date.now();
+
+		    // The emitter requires the elapsed
+		    // number of seconds since the last update
+		    emitter.update((now - elapsed) * 0.001);
+		    elapsed = now;
+
+		    // Should re-render the PIXI Stage
+		    // renderer.render(stage);
+		};
+
+		// Start emitting
+		emitter.emit = true;
+
+		// Start the update
+		update();*/
+	});
 });
+
+
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,94 +1,146 @@
 $(document).ready(function() {
 
-	var limit = 10, count = 0;
-    var container = document.getElementById('content'),
-   		socket = io.connect('http://localhost:8080');
-   	var score = 0;
-   	var maxScore = localStorage.getItem("score");
+	var limit = 16, count = 0,
+		score = 0, maxScore = localStorage.getItem("score"),
+		displayingEnabled = true,
+    	socket = io.connect('http://localhost:8080');
 
-   	document.getElementById("max_score").innerHTML = maxScore;
+    // Dimensions for tweets
+ 	var width=300, height=200, padding=20,
+ 		headerHeight = $('header').css('height'),
+ 		headerMargin = $('header').css('marginBottom');
+
+ 	headerHeight = headerHeight.substring(0, headerHeight.length - 2);
+ 	headerMargin = headerMargin.substring(0, headerMargin.length - 2);
+
+ 	// General dimensions
+ 	var windowWidth  = $(window).width(),
+ 		windowHeight = $(window).height();
+
+   	// PIXI.js aliases
+   	var Container = PIXI.Container,
+   		autoDetectRenderer = PIXI.autoDetectRenderer,
+    	loader = PIXI.loader,
+    	resources = PIXI.loader.resources,
+    	Sprite = PIXI.Sprite,
+    	Text = PIXI.Text,
+    	Graphics = PIXI.Graphics,
+    	Stage = PIXI.Stage,
+    	Rectangle = PIXI.Rectangle;
+
+    document.getElementById("max_score").innerHTML = maxScore;
+	
+	// Create renderer
+	var renderer = autoDetectRenderer(windowWidth - 7, windowHeight - 7 - headerHeight - headerMargin, null, {transparent:true});
+	renderer.backgroundColor = 0xFFFFFF;
+
+	// Add the canvas to the HTML document
+	document.getElementById("content").appendChild(renderer.view);
+
+	// Create a container object called `stage` in which we will add the tweets
+	var stage = new Stage();
 
 
-   	function textWrapper(txt, width, margin, ctx){
+    /**
+     * [displayMessage : add text content on container]
+     * @param  {[type]} stage     [container in which the text will appear]
+     * @param  {[type]} message   [text content]
+     * @param  {[type]} font      [font family]
+     * @param  {[type]} font_size [font size]
+     * @param  {[type]} color     [text color]
+     * @param  {[type]} width     [maximum width of the text content]
+     * @param  {Number} x         [x position for top left anchor]
+     * @param  {Number} y         [y position for top left anchor]
+     * @return {[type]}           [message that needs to be displayed]
+     */
+   	function displayMessage(stage, message, font, font_size, color, width, x=0, y=0){
 
-   		var bufferLine = "", testLine = "", size;
-   		var words = txt.split(' ');
-   		var offset = 0;
-   		width = width - (margin * 2);
+		var messageToDisplay = new Text(
+		 	message,
+		 	{fontFamily: font, fontSize: font_size, fill: color, wordWrap: true, wordWrapWidth: width}
+		);
 
-   		ctx.fillStyle = "black";
-   		ctx.font = "15px Anonymous Pro";
+		messageToDisplay.position.set(x, y);
+		return messageToDisplay;
 
-   		for( var i=0; i<words.length; i++ ){
-   			
-   			bufferLine += words[i];
-
-   			testLine = bufferLine + words[i+1] + " ";
-   			size = ctx.measureText(testLine);
-
-   			if(size.width > width){
-
-   				ctx.fillText( bufferLine, margin, 70+(offset*17) );
-   				offset = offset + 1;
-   				bufferLine = "";
-
-   			}else bufferLine += " ";
-
-   		}
    	}
-                    
+
+
+   	/**
+   	 * [addPicture : Load and display picture]
+   	 * @param {[type]} renderer [current render view]
+   	 * @param {[type]} stage    [current stage object used]
+   	 * @param {[type]} url      [picture link]
+   	 */
+   	function addPicture(renderer, stage, url){
+
+   		loader
+		  .add(url)
+		  .load(setup);
+
+		function setup() {
+
+		  var avatar = new Sprite(resources[url].texture);  
+		  stage.addChild(avatar);
+		  displayingEnabled = true;
+
+		}
+
+   	}
+
+
+    /**
+     * [main function. Called each time a tweet is streamed. Display it on the view and init interaction]
+     * @param  {[type]} data [tweet content]
+     */
     socket.on('twitter', function(data) {
-    	
-    	if(count < limit){
 
-    		$('#content').prepend('<canvas id="'+data.id +'" class="tweet"></canvas>');
+    	/** [update : handle render and animation] */
+		function update(){
+		    requestAnimationFrame(update);
+		    renderer.render(stage);
+		};
+	
 
-	    	var canvas = document.getElementById( data.id );
-			var ctx = canvas.getContext("2d");
+    	if(displayingEnabled == true && count < limit){
 
-			//var padding = $(canvas).css('padding');
-			//padding = padding.substring( 0, padding.length - 2 );
+    		displayingEnabled = false;
 
-			var padding = 10;
 
-			ctx.fillStyle="rgba(20,20,20, .8)";
-			ctx.fillRect(0,0,canvas.offsetWidth, 48);
-				    	
-			ctx.font = "25px VT323";
-			ctx.fillStyle = "white";
-			ctx.textAlign = "start";
+			// Load outline content
+			var outline = new Graphics();
+			outline.lineStyle(1, 0x333333, 1);
+			outline.beginFill(0xFFFFFF, .8);
+			outline.drawRect(0, 0, width-1, height-1);
+			outline.interactive = true;
+			outline.hitArea = new Rectangle(0, 0, width-1, height-1);
+			outline.endFill();
 
-			ctx.fillText( data.user.screen_name, 70, 30); 
-			
-			textWrapper(
-				data.text,
-				canvas.offsetWidth,
-				padding,
-				ctx);
 
-			var img = new Image();
-			img.onload = function()
-			{
-			    ctx.drawImage(img, 0, 0, img.width, img.height);
-			};
-						
-		    img.src = data.user.profile_image_url;
+			// Load header
+			var rectangle = new Graphics();
+			rectangle.beginFill(0x333333);
+			rectangle.drawRect(0, 0, width, 48);
+			rectangle.endFill();
 
-		    count++;
 
-		        $(canvas).on('click', function(){
+			// Load profile name and tweet content
+			var tweetUserName = displayMessage(stage, data.user.screen_name, "VT323", "25px", "white", width, 60, 15);
+			var tweetContent = displayMessage(stage, data.text, "Anonymous Pro", "16px", "black", width - (padding * 2), padding, 	70);
 
-					score ++;
-					if(score > maxScore){
-						localStorage["score"] = score;
-						document.getElementById("max_score").innerHTML = score;
 
-					}
-					document.getElementById("score").innerHTML = score;
+			// Add all element to the stage
+			stage.addChild(outline, rectangle, tweetUserName, tweetContent);
 
-					canvas.remove();
 
-				});
+			// Add picture
+			addPicture(renderer, stage, data.user.profile_image_url);
+
+			count++;
+
+
+			// Call update function to render view
+			update();
 
     	}
     	
@@ -96,4 +148,3 @@ $(document).ready(function() {
 
 
 });
-
